74d35267087876835a05477616234e9f93667734
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2023-11-02 15:22:57 +0100

    refactor: use [ instead [[
    Accept the comment of asperan on https://github.com/asperan/check-conventional-commits-action/pull/1\#discussion_r1379806337

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ fi
 ERRORS="$(git log --grep='^\w+(\((\w|-)+\))?!?: .+$' --invert-grep -E --pretty=format:%s | awk 'END {print NR}')"
 echo "$ERRORS"
 
-if [[ -n $GITHUB_ACTIONS && $GITHUB_ACTIONS == true ]]; then
+if [ -n $GITHUB_ACTIONS ] && [ "$GITHUB_ACTIONS" == "true" ]; then
     # Print to github_output only in a Github Actions environment.
     echo "errors=$ERRORS" >> "$GITHUB_OUTPUT"
 fi
